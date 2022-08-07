3dda9c5ca7b0d377f89a511297bb788a6383e103
Italo A <italoaurelior@gmail.com>
2022-08-07 19:16:06 -0300

    docs: document canvas and colors

--- includes/canvas.h
+++ includes/canvas.h
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 21:19:49 by vwildner          #+#    #+#             */
-/*   Updated: 2022/08/07 18:27:20 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/08/07 19:15:46 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,14 @@ typedef struct s_color {
 	unsigned int	rgb; // Decimal representation of the color
 }	t_color;
 
+/**
+ * @brief A struct to represent canvas in screen based on pixels.
+ *
+ */
 typedef struct s_canvas {
-	unsigned int	width;
-	unsigned int	height;
-	t_color			***pixels;
+	unsigned int	width; // Width of the canvas
+	unsigned int	height; // Height of the canvas
+	t_color			***pixels; // Color of each pixel in the canvas
 }	t_canvas;
 
 typedef struct s_ppm {
@@ -82,11 +86,42 @@ t_color			*multiply_scalar_color(t_color *c, double v);
  */
 t_color			*multiply_colors(t_color *a, t_color *b);
 
+/**
+ * @brief Create canvas with each pixel set to black
+ * @param width width of canvas
+ * @param height height of canvas
+ * @return t_canvas* created canvas struct
+*/
 t_canvas		*new_canvas(int width, int height);
 
 unsigned int	merge_colors(double r, double g, double b);
+
+/**
+ * @brief convert given color to RGB format
+ * @param c color float value
+ * @return unsigned int RGB value
+ */
 unsigned int	to_rgb(double c);
 
 int				write_ppm(t_canvas *c, char *filename);
 
+/**
+ * @brief write pixels into canvas
+ * @param c canvas struct to be used
+ * @param width width position of pixel
+ * @param height height position of pixel
+ * @param color color of pixel
+ * @return int 0 if successful, 1 if unsuccessful
+ */
+int	write_pixel(t_canvas *c, unsigned int width, unsigned int height,
+				t_color *color);
+
+/**
+ * @brief get pixel color at specific position on canvas
+ * @param c canvas struct to be used
+ * @param width width position of pixel
+ * @param height height position of pixel
+ * @return t_color* color of pixel or NULL if unsuccessful
+ */
+t_color	*pixel_at(t_canvas *c, unsigned int width, unsigned int height);
 #endif
