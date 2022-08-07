b4ef46552e1e786dee5f7c2a00955e4c5b5626b7
Victor Wildner <vwildner@redhat.com>
2022-08-07 19:14:19 -0300

    docs: document the canvas

--- includes/canvas.h
+++ includes/canvas.h
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 21:19:49 by vwildner          #+#    #+#             */
-/*   Updated: 2022/08/07 18:31:40 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/08/07 19:13:52 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@
 # include <libft.h>
 # include <fcntl.h>
 
+/**
+ * @brief A tuple of three components.
+ * Red, green, and blue and their respective merged components.
+ *
+ */
 typedef struct s_color {
 	double			r;
 	double			g;
@@ -28,28 +33,115 @@ typedef struct s_color {
 	unsigned int	rgb;
 }	t_color;
 
+/**
+ * @brief The canvas structure. It has a width and a height, and a pointer
+ * to a matrix of colors that are called pixels.
+ */
 typedef struct s_canvas {
 	unsigned int	width;
 	unsigned int	height;
 	t_color			***pixels;
 }	t_canvas;
 
+/**
+ * @brief A ppm representation of a canvas.
+ * The ppm has a header and a data component.
+ * The header has information about the width, height, and color depth.
+ * The data has the actual pixels.
+ */
 typedef struct s_ppm {
 	char	*header;
 	u_int	*data;
 }	t_ppm;
 
+/**
+ * @brief Creates a new color.
+ *
+ * @param r The red component of the color.
+ * @param g The green component of the color.
+ * @param b The blue component of the color.
+ * @return t_color* Returns a pointer to the new color.
+ */
 t_color			*new_color(double r, double g, double b);
+
+/**
+ * @brief Adds two colors together and returns the result.
+ *
+ * @param a The first color.
+ * @param b The second color.
+ * @return t_color* Returns the result of the addition.
+ */
 t_color			*add_colors(t_color *a, t_color *b);
+
+/**
+ * @brief Subtracts two colors
+ *
+ * @param a The first color
+ * @param b The second color
+ * @return t_color* Returns the difference of the two colors
+ */
 t_color			*subtract_colors(t_color *a, t_color *b);
+
+/**
+ * @brief Multiplies a color by a scalar value.
+ *
+ * @param c The color to multiply.
+ * @param v The scalar value.
+ * @return t_color* Returns the resulting color.
+ */
 t_color			*multiply_scalar_color(t_color *c, double v);
+
+/**
+ * @brief Multiplies two colors
+ *
+ * @param a The first color
+ * @param b The second color
+ * @return t_color* Returns a new color with the result of the multiplication
+ */
 t_color			*multiply_colors(t_color *a, t_color *b);
 
+/**
+ * @brief Creates a new instance of a canvas.
+ *
+ * @param width The width of the canvas.
+ * @param height The height of the canvas.
+ * @return t_canvas* Returns a pointer to the new canvas.
+ */
 t_canvas		*new_canvas(int width, int height);
 
+/**
+ * @brief Creates a merged integer representation
+ *
+ * @param r The float red component
+ * @param g The float green component
+ * @param b The float blue component
+ * @return unsigned int Returns the merged integer representation
+ */
 unsigned int	merge_colors(double r, double g, double b);
+
+/**
+ * @brief Transforms a floating number to a 0 - 255 integer representation.
+ *
+ * @param c The number to transform.
+ * @return unsigned int The transformed number.
+ */
 unsigned int	to_rgb(double c);
 
+/**
+ * @brief Writes a PPM representation of the canvas to the given file.
+ *
+ * @param c The canvas to write.
+ * @param filename The name of the file to write to.
+ * @return int Returns 0 on success, > 0 on failure.
+ */
 int				write_ppm(t_canvas *c, char *filename);
+
+/**
+ * @brief Transforms a canvas into a PPM file representation.
+ *
+ * @param c The canvas to transform.
+ * @return t_ppm* The PPM representation.
+ */
+t_ppm			*canvas_to_ppm(t_canvas *c);
 
 #endif
