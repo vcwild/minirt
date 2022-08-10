0a02a7ae1cc7ab3c2d5474705809c3b4c82e2adb
Italo <italoaurelior@gmail.com>
2022-08-09 21:20:29 -0300

    docs: add documentation and notes from resources (#2)
    * docs: add more resource to readme
    
    * feat: document functions reading book
    
    * docs: add more content into readme
    
    * docs: add help steps
    
    * docs: change comand to submodule init
    
    * docs: add notes from book
    
    * docs: remove redundant this
    
    * docs: add more comments
    
    * docs: colors
    
    * docs: document canvas and colors
    
    * docs: remove books notes
    
    * docs: update readme

--- README.md
+++ README.md
@@ -25,6 +25,19 @@ A ray tracer is a program that takes a scene and a camera and renders the scene 
 
 These are the dependencies required for development:
 
+### Dependencies
+
+Install libs
+
+```sh
+sudo apt-get update && sudo apt-get install xorg libxext-dev zlib1g-dev libbsd-dev
+```
+
+If you want to run tests on project clone munit into /tests folder
+```sh
+git submodule init
+```
+
 ### Create a venv
 
 ```python3
@@ -52,16 +65,30 @@ pre-commit install --install-hooks --overwrite
 
 1. Cast a ray into the scene, and find where it strikes a surface.
 2. Cast a ray from that point toward each light source to determine which
-lights illuminate that point.
+   lights illuminate that point.
 3. If the surface is reflective, cast a new ray in the direction of reflection and
-recursively determine what color is reflected there.
+   recursively determine what color is reflected there.
 4. If the surface is transparent, do the same thing in the direction of refrac-
-tion.
+   tion.
 5. Combine all colors that contribute to the point (the color of the surface,
-the reflection, and refraction) and return that as the color of the pixel.
+   the reflection, and refraction) and return that as the color of the pixel.
+
+This project uses minilibx-linux
+MinilibX, a simple X-Window (X11R6) programming API in C, designed for students, suitable for X-beginners.
+
+Unofficial documentation for minilibx
+
+- [Harm Smiths Docs](https://harm-smits.github.io/42docs/libs/minilibx)
+- [Getting started with the minilibx](https://aurelienbrabant.fr/blog/getting-started-with-the-minilibx)
+
 
 ## References
 
 - [The Ray Tracer Challenge](https://www.raytracing.org/raytracerchallenge/)
 - [How Ray Tracing Works](https://www.youtube.com/watch?v=lKIytgt3KXM)
 - [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
+- [Simple Method for Creating 3D Images](https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-ray-tracing)
+- [Khan Academy Rendering](https://www.khanacademy.org/computing/pixar/rendering)
+- [Notion 1 Resource](https://www.notion.so/miniRT-5f6fcdf6d05e4742b6c38f0588f12436)
+- [Disney's Practical Guide to Path Tracing](https://www.youtube.com/watch?v=frLwRLS_ZR0)
+- [An Intuitive Guide to Linear Algebra](https://betterexplained.com/articles/linear-algebra-guide/)
--- includes/tuple.h
+++ includes/tuple.h
@@ -3,49 +3,180 @@
 /*                                                        :::      ::::::::   */
 /*   tuple.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 11:40:13 by vwildner          #+#    #+#             */
-/*   Updated: 2022/08/06 18:29:23 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/08/07 17:56:04 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TUPLE_H
-# define TUPLE_H
-# define EPSILON 0.0000001
+#define TUPLE_H
+#define EPSILON 0.0000001
 # define TUPLE_DECIMAL_PRECISION 5
-# include <stdlib.h>
-# include <math.h>
 
-typedef struct s_tuple {
-	double	x;
-	double	y;
-	double	z;
-	double	w;
-}	t_tuple;
+#include <stdlib.h>
+#include <math.h>
 
-t_tuple	*new_tuple(double x, double y, double z, double w);
-t_tuple	*new_point(double x, double y, double z);
-t_tuple	*new_vector(double x, double y, double z);
+/**
+ * Stores a tuple for 3D space.
+*/
+typedef struct s_tuple
+{
+	double x; // x axis pointing to the right
+	double y; // y axis point up
+	double z; // z axis pointing out of the screen
+	double w; // w defines if the tuple is a point, vector or tuple
+} t_tuple;
 
-int		is_point(t_tuple *t);
-int		is_vector(t_tuple *t);
-int		is_tuple(t_tuple *t);
-int		dequals(double a, double b);
-int		tuple_equals(t_tuple *a, t_tuple *b);
+/**
+ * @brief create a new tuple
+ * based on the given x, y, z values
+ * @param x x axis value
+ * @param y y axis value
+ * @param z z axis value
+ * @param w w type of the tuple
+ * @return a pointer to tuple created
+ */
+t_tuple *new_tuple(double x, double y, double z, double w);
 
-t_tuple	*add_tuples(t_tuple *a, t_tuple *b);
-t_tuple	*subtract_tuples(t_tuple *a, t_tuple *b);
-t_tuple	*negate_tuple(t_tuple *a);
-t_tuple	*multiply_scalar(t_tuple *t, double v);
-t_tuple	*divide_scalar(t_tuple *t, double v);
+/**
+ * @brief create a point tuple (w = 1.0)
+ * based on the given x, y, z values
+ * @param x x axis value
+ * @param y y axis value
+ * @param z z axis value
+ * @return a pointer to tuple created
+ */
+t_tuple *new_point(double x, double y, double z);
 
-double	dot(t_tuple *a, t_tuple *b);
-double	magnitude(t_tuple *a);
-t_tuple	*normalize(t_tuple *a);
-t_tuple	*cross(t_tuple *a, t_tuple *b);
-t_tuple	*reflect(t_tuple *v, t_tuple *n);
+/**
+ * @brief create a vector tuple  (w = 0.0)
+ * based on the given x, y, z values
+ * @param x x axis value
+ * @param y y axis value
+ * @param z z axis value
+ * @return a pointer to tuple created
+ */
+t_tuple *new_vector(double x, double y, double z);
 
+/**
+ * @brief check if tuple is a point
+ * tuple is a point if w is 1.0
+ * @param t tuple to be checked
+ * @return true if tuple is a point, false otherwise
+ */
+int is_point(t_tuple *t);
+
+/**
+ * @brief check if tuple is a vector
+ * tuple is a vector if w is 0.0
+ * @param t tuple to be checked
+ * @return true if tuple is a vector, false otherwise
+ */
+int is_vector(t_tuple *t);
+
+int is_tuple(t_tuple *t);
+
+/**
+ * @brief check if two doubles are equals
+ * using the EPSILON value to avoid rounding errors
+ * @param a first double to be compared
+ * @param b second double to be compared
+ * @return true if equals, false otherwise
+ */
+int dequals(double a, double b);
+
+/**
+ * @brief check if two tuples are equals
+ * using the dequals function
+ * @param a first tuple to be compared
+ * @param b second tuple to be compared
+ * @return true if equals, false otherwise
+ */
+int tuple_equals(t_tuple *a, t_tuple *b);
+
+/**
+ * @brief make a new tuple that is the sum of two tuples,
+ * this allow you to know where you would be if you followed
+ * the vector from that point
+ * @param a first tuple to be added
+ * @param b second tuple to be added
+ * @return a pointer to the new tuple
+ */
+t_tuple *add_tuples(t_tuple *a, t_tuple *b);
+
+/**
+ * @brief make a new tuple that is the difference of two tuples,
+ * this allow you to know where you would be if you followed
+ * the vector from that point. Useful when you need to find
+ * the vector that points to your light source
+ * @param a first tuple to be subtracted
+ * @param b second tuple to be subtracted
+ * @return a pointer to the new tuple
+ */
+t_tuple *subtract_tuples(t_tuple *a, t_tuple *b);
+
+/**
+ * @brief invert the sign of the tuple values
+ * to get the opposite vector
+ * @param a tuple to have values inverted
+ * @return a pointer to the inverted tuple
+ */
+t_tuple *negate_tuple(t_tuple *a);
+
+/**
+ * @brief multiply a tuple by a scalar values
+ * to get uniform scaling
+ * @param t tuple to be multiplied
+ * @param v scalar value
+ * @return the tuple values multiplied by the scalar value
+ */
+t_tuple *multiply_scalar(t_tuple *t, double v);
+
+/**
+ * @brief divide a tuple by a scalar values
+ * to get uniform scaling
+ * @param t tuple to be divided
+ * @param v scalar value
+ * @return the tuple values divided by the scalar value
+ */
+t_tuple *divide_scalar(t_tuple *t, double v);
+
+
+/**
+ * @brief calculate the angle between two vectors
+ * @param a first tuple
+ * @param b second tuple
+ * @return double scalar value of the angle between the two vectors
+ */
+double dot(t_tuple *a, t_tuple *b);
+
+/**
+ * @brief calculate magnitude of a vector, it's
+ * how far it is from the origin
+ * @param a tuple to be calculated
+ * @return magnitude of the vector
+ */
+double magnitude(t_tuple *a);
+
+/**
+ * @brief normalize a vector, it's
+ * make the vector have a magnitude of 1.0 (unit vector)
+ * @param a tuple to be normalized
+ * @return a pointer to the normalized tuple
+ */
+t_tuple *normalize(t_tuple *a);
+
+/**
+ * @brief calculate the cross product of two vectors
+ * @param a first tuple
+ * @param b seconde tuple
+ * @return t_tuple* vector that is the cross product of the two tuples
+ */
+t_tuple *cross(t_tuple *a, t_tuple *b);
+
+t_tuple *reflect(t_tuple *v, t_tuple *n);
 double	round_to(double num);
 
 #endif
