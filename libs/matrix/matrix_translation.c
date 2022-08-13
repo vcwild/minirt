0decb5a422e6eef3e71b5dcb4ba5b47a9de6d00a
Italo A <italoaurelior@gmail.com>
2022-08-13 11:15:57 -0300

    refactor: translation with identity

--- libs/matrix/matrix_translation.c
+++ libs/matrix/matrix_translation.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 10:28:55 by itaureli          #+#    #+#             */
-/*   Updated: 2022/08/13 10:43:22 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/08/13 11:15:41 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,8 @@
 t_matrix	*translation(double x, double y, double z)
 {
 	t_matrix	*new;
-	int			i;
-	int			j;
 
-	new = new_matrix(4, NULL);
-	i = -1;
-	while (++i < 4)
-	{
-		j = -1;
-		while (++j < 4)
-		{
-			if (i == 3 && j == 3)
-				new->data[i][j] = 1;
-			else if (i == j)
-				new->data[i][j] = 1;
-			else
-				new->data[i][j] = 0;
-		}
-	}
+	new = identity_matrix();
 	new->data[0][3] = x;
 	new->data[1][3] = y;
 	new->data[2][3] = z;
