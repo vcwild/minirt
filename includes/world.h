634658c30acf5ac87c405117cf7cd957375ee298
Italo A <italoaurelior@gmail.com>
2022-08-21 21:24:09 -0300

    docs: document lib

--- includes/world.h
+++ includes/world.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   world.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 09:10:04 by itaureli          #+#    #+#             */
-/*   Updated: 2022/08/21 17:29:54 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/08/21 21:23:46 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,29 @@
 # include <light.h>
 # include <stdbool.h>
 
+/**
+ * @brief Struct of objects
+ *
+ */
 typedef struct s_objects {
 	int				total;
 	int				sphere_count;
 	t_list			*spheres;
 }	t_objects;
 
+/**
+ * @brief The world struct with all the objects and lights.
+ *
+ */
 typedef struct s_world {
 	t_objects	objects;
 	t_list		*lights;
 }	t_world;
 
+/**
+ * @brief Struct with precomputed values for the intersection.
+ *
+ */
 typedef struct s_computations {
 	double			t;
 	t_object_type	object_type;
@@ -37,17 +49,78 @@ typedef struct s_computations {
 	bool			inside;
 }					t_computations;
 
+/**
+ * @brief Creates a new empty world, with no objects and light source.
+ *
+ * @return t_world* The new world.
+ */
 t_world			*new_world(void);
+
+/**
+ * @brief Creates a new empty object list.
+ *
+ * @return t_objects* The new object list.
+ */
 t_objects		*new_objects(void);
+
+/**
+ * @brief Creates a new world with a single light source and two spheres.
+ *
+ * @return t_world* The new default world.
+ */
 t_world			*default_world(void);
+
+/**
+ * @brief A function that calculates the intersection of a ray into
+ * objects in a world.
+ *
+ * @param w The world to calculate the intersection in.
+ * @param r The ray to calculate the intersection for.
+ * @return t_intersections* Sortered list of intersections in the
+ * world.
+ */
 t_intersections	*intersect_world(t_world *w, t_ray *r);
 
+/**
+ * @brief Add a new light to the world.
+ *
+ * @param w The world to add the light to.
+ * @param pl The light to add.
+ * @return int 0 if success, -1 if error.
+ */
 int				add_light(t_world *w, t_point_light *pl);
+
+/**
+ * @brief Add a new sphere to the world.
+ *
+ * @param w The world to add the sphere to.
+ * @param sphere The sphere to add.
+ * @return int 0 if success, -1 if error.
+ */
 int				add_sphere(t_world *w, t_shape *sphere);
 
+/**
+ * @brief Precomputes the intersection values for the given intersection.
+ *
+ * @param x The intersection to precompute.
+ * @param r The ray that intersected the intersection.
+ * @return t_computations* New data structure encapsulating some precomputed
+ * information about the intersection.
+ */
 t_computations	*prepare_computations(t_intersection *x, t_ray *r);
 
+/**
+ * @brief Destroys a world and all its objects and lights.
+ *
+ * @param w The world to destroy.
+ */
 void			destroy_world(t_world *w);
+
+/**
+ * @brief Destroys precomputed intersection data.
+ *
+ * @param c The data to destroy.
+ */
 void			destroy_computations(t_computations *c);
 
 #endif
