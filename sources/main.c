e91b7def63665fa04b2217a22ffd0da75effe1a9
Italo A <italoaurelior@gmail.com>
2022-09-08 20:20:43 -0300

    refactor: error based on pdf

--- sources/main.c
+++ sources/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 18:56:01 by vwildner          #+#    #+#             */
-/*   Updated: 2022/09/07 20:13:42 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/09/08 20:20:22 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int	main(int argc)
 	t_rt	*rt;
 
 	if (argc != 2)
-		return (ft_putstr_fd("Error: Wrong nb arguments", STDERR_FILENO), 1);
+		return (ft_putstr_fd("Error \n Wrong number of arguments",
+				STDERR_FILENO), 1);
 	rt = new_ray_tracer();
 	destroy_ray_tracer(rt);
 	return (0);
